aae315efd2be3cbbc38ed8a5fb114f4d02dd7571
Yiğitcan Ölmez <yigitcanolmez@outlook.com>
2023-01-21 15:21:07 +0300

    CategoryService Added

--- Business/Abstract/ICategoryService.cs
+++ Business/Abstract/ICategoryService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace Business.Abstract
 {
     public interface ICategoryService
     {
+        List<Category> GetAll();
+        Category GetById(int categoryId);
+
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -6,3 +6,19 @@ using Entities.Concrete;
 
 
 ProductService productService = new ProductService(new EfProductDal());
+CategoryService categoryService = new CategoryService(new EfCategoryDal());
+
+
+foreach (var product in productService.GetByUnitPrice(40, 100))
+{
+    var categoryName = categoryService.GetById(product.CategoryId);
+    Console.WriteLine("{0} Price : {1} Stock Count :{2} Category Name : {3} ", product.ProductName, product.UnitPrice, product.UnitsInStock, categoryName.CategoryName);
+}
+
+
+
+//foreach (var category in categoryService.GetAll())
+//{
+//    Console.WriteLine("Category Name: " + category);
+
+//}
